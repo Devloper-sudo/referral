d9f8483040b14f1c8380043e723d89d871da39c6
abhisheks-iprogrammer <abhisheks@iprogrammer.com>
2021-05-25 10:58:52 +0530

    First COmmit

---
+++ referral/src/main/java/com/reign/referral/LogDebug.java
@@ -0,0 +1,21 @@
+package com.reign.referral;
+
+import android.util.Log;
+
+public class LogDebug {
+
+
+    private static final String REFERRAL = "referral";
+
+    public static void d(String messgae) {
+        Log.d(REFERRAL, messgae);
+    }
+
+}
+1-git init
+        2-git add .
+        3.git commit -m "First Commit"
+        4.Copy the Url
+        5.git remote add origin "remote repository URL"
+        6.git remote -v
+        7.git push origin master
